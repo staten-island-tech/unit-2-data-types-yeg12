55a519378fa6191f03365c02ca8f7341b904df83
yeg12 <yeg@nycstudents.net>
2026-02-03 10:17:48 -0500

    Create 6767676767676767.py

---
+++ 6767676767676767.py
@@ -0,0 +1,12 @@
+def discount(isMember, age, isResident):
+if  age <=(18):
+    print(discount)
+if age <=( 65):
+    print(discount)
+if isResident ==(True):
+    print(discount)
+if isMember ==(True):
+    print(discount)
+
+
+
